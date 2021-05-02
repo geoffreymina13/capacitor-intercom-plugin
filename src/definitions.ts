662df8e32d09453c2f2602a49f63b973543b7878
Geoff Mina <geoffreymina@gmail.com>
2021-05-02 13:37:40 -0600

    Functional Capacitor Plugin

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,36 @@
 declare module '@capacitor/core' {
   interface PluginRegistry {
-    Intercom: IntercomPlugin;
+    IntercomPlugin?: IntercomProtocol;
   }
 }
 
-export interface IntercomPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+export interface IntercomProtocol {
+  registerIdentifiedUser(options: {
+    userId?: string;
+    email?: string;
+  }): Promise<void>;
+  registerUnidentifiedUser(): Promise<void>;
+  updateUser(options: UserUpdateOptions): Promise<void>;
+  logout(): Promise<void>;
+  logEvent(options: { name: string; data?: any }): Promise<void>;
+  displayMessenger(): Promise<void>;
+  displayMessageComposer(options: { message: string }): Promise<void>;
+  displayHelpCenter(): Promise<void>;
+  hideMessenger(): Promise<void>;
+  displayLauncher(): Promise<void>;
+  hideLauncher(): Promise<void>;
+  displayInAppMessages(): Promise<void>;
+  hideInAppMessages(): Promise<void>;
+  setUserHash(options: { hmac: string }): Promise<void>;
+  setBottomPadding(options: { value: string }): Promise<void>;
 }
+
+export interface UserUpdateOptions {
+  userId?: string;
+  email?: string;
+  name?: string;
+  phone?: string;
+  languageOverride?: string;
+  customAttributes: { [key: string]: any };
+}
+
